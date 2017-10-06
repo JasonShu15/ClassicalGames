cdd01eecbc7f37e3cff39ed63d818aa7d69da3e3
Shuhaojie <2386677465@qq.com>
2017-10-06 21:33:30 +0800

    Create Game.java

---
+++ Snake/Game.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+import static com.sun.glass.ui.Cursor.setVisible;
+
+public class Game extends JFrame {
+    public snakeMain() {
+        Controller ctrl = new Controller();
+        add(ctrl);
+        JFrame jf=new JFrame();
+        setTitle("̰贪吃蛇");
+        setSize(435,390);
+        setLocation(200, 200);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Game();
+    }
+}
